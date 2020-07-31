cdf401396b43412fb78f1d5ad59fa1f65918fbd0
wealiar <wealiar@gmail.com>
2020-07-31 22:56:14 +0600

    Updates

---
+++ precache-manifest.9036c00e3aa2170b0ad9b49f82c99801.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403a9f5110150999a878cfab46c39b82",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7d7b23bd781757c507df",
+    "url": "./static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "79dbe82c78a8abdd9f07",
+    "url": "./static/css/main.1b00e0e1.chunk.css"
+  },
+  {
+    "revision": "7d7b23bd781757c507df",
+    "url": "./static/js/2.6c269fc2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.6c269fc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79dbe82c78a8abdd9f07",
+    "url": "./static/js/main.ac19aa41.chunk.js"
+  },
+  {
+    "revision": "6bce63460a7b1988ceb7",
+    "url": "./static/js/runtime-main.4feb9dfc.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "./static/media/SEGA.12a3db8b.woff"
+  }
+]);
